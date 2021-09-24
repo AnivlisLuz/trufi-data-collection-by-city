b05a4dcc7e58511687978f244a82520f38721f6c
Samuel Rioja <samuelriotz@hotmail.com>
2021-09-24 10:46:31 -0400

    clean routes and add duitama gtfs

--- data-builder/run.sh
+++ data-builder/run.sh
@@ -6,8 +6,8 @@ mkdir ./temp/otp
 
 wget -O ./input.osm.pbf "https://download.geofabrik.de$geofabrik_url_path"
 
-osmium extract --bbox=$bbox  --set-bounds  ./input.osm.pbf  --output "./temp/otp/$city.osm.pbf"
+osmium extract --bbox=$bbox  --set-bounds  ./input.osm.pbf  --output "./temp/otp/city.osm.pbf"
 
-./tilemaker/build/tilemaker --input "./temp/otp/$city.osm.pbf" --output "./temp/tiles/$city.mbtiles" --process ./tilemaker/resources/process-openmaptiles.lua --config ./tilemaker/resources/config-openmaptiles.json
+./tilemaker/build/tilemaker --input "./temp/otp/city.osm.pbf" --output "./temp/tiles/city.mbtiles" --process ./tilemaker/resources/process-openmaptiles.lua --config ./tilemaker/resources/config-openmaptiles.json
 [ -d "./out/$city" ] || mkdir "./out/$city"
 cp -a ./temp/. ./out/$city
