6466ffb456fb3abf6b2acf1bf4258be2e663fc86
Samuel Rioja <samuelriotz@hotmail.com>
2021-09-27 12:32:37 -0400

    adding data builder

--- data-builder/run.sh
+++ data-builder/run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 rm -rf ./temp/
 mkdir ./temp/
-mkdir ./temp/tiles
 mkdir ./temp/otp
 
 wget -O ./input.osm.pbf "https://download.geofabrik.de$geofabrik_url_path"
 
 osmium extract --bbox=$bbox  --set-bounds  ./input.osm.pbf  --output "./temp/otp/city.osm.pbf"
 
-./tilemaker/build/tilemaker --input "./temp/otp/city.osm.pbf" --output "./temp/tiles/city.mbtiles" --process ./tilemaker/resources/process-openmaptiles.lua --config ./tilemaker/resources/config-openmaptiles.json
+[ -d "./tiles/$city" ] || mkdir "./tiles/$city"
+./tilemaker/build/tilemaker --input "./temp/otp/city.osm.pbf" --output "./tiles/$city/city.mbtiles" --process ./tilemaker/resources/process-openmaptiles.lua --config ./tilemaker/resources/config-openmaptiles.json
+
 [ -d "./out/$city" ] || mkdir "./out/$city"
 cp -a ./temp/. ./out/$city
